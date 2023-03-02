084ca6bf3339ec4358f938eff40808098cd03fdb
Samila Vieira <samilav1997@gmail.com>
2023-03-01 22:56:08 -0300

    Criando state global

--- src/stores/projectSlicer.ts
+++ src/stores/projectSlicer.ts
@@ -1,7 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
 
-interface PropsProject {
+interface State {
+  projects: Project[];
+  selectedProjectId: string;
+}
+interface Project {
   id: string;
   name: string;
   tasks: TaskProps[];
@@ -17,13 +21,21 @@ interface TaskProps {
   //state?: "Novo" | "Em Andamento" | "Pronto";
   //tags?: string[];
 }
-const initialState = [{ id: "123", name: "Inbox", tasks: [] }];
+const inboxProject = { id: uuidv4(), name: "Inbox", tasks: [] };
+const initialState: State = {
+  projects: [inboxProject],
+  selectedProjectId: inboxProject.id,
+};
+
 export const projectSlicer = createSlice({
   name: "projects",
   initialState,
   reducers: {
+    setSelectedProjectId: (state, action) => {
+      state.selectedProjectId = action.payload;
+    },
     createNewProject: (state, action) => {
-      state.push({
+      state.projects.push({
         id: uuidv4(),
         name: action.payload,
         tasks: [],
@@ -31,14 +43,16 @@ export const projectSlicer = createSlice({
     },
 
     deleteProject: (state, action) => {
-      const copyProjects = [...state];
+      const copyProjects = [...state.projects];
       console.log({ action });
-      state = copyProjects.filter((project) => project.id == action.payload.id);
+      state.projects = copyProjects.filter(
+        (project) => project.id == action.payload.id
+      );
     },
 
     createNewTask: (state, action) => {
-      const copyProjects = [...state];
-      const projecteSelectedIndex = state.findIndex(
+      const copyProjects = [...state.projects];
+      const projecteSelectedIndex = state.projects.findIndex(
         (project) => project.id === action.payload.id
       );
       const currectProject = copyProjects[projecteSelectedIndex];
