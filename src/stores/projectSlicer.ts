2aa474000ede1224645a9876d3e79af8aa017b27
Samila Vieira <samilav1997@gmail.com>
2023-03-06 13:24:46 -0300

    adicionado mérodo global delete

--- src/stores/projectSlicer.ts
+++ src/stores/projectSlicer.ts
@@ -12,14 +12,11 @@ interface Project {
 }
 
 interface TaskProps {
+  id: string;
   title: string;
-  //description?: string;
-  //completed?: boolean;
-  //startDate?: Date;
-  //dueDate?: Date;
-  priority?: "Baixa" | "Normal" | "Alta";
-  //state?: "Novo" | "Em Andamento" | "Pronto";
-  //tags?: string[];
+  description?: string;
+  startDate?: string;
+  //priority?: "Baixa" | "Normal" | "Alta";
 }
 const inboxProject = { id: uuidv4(), name: "Inbox", tasks: [] };
 const initialState: State = {
@@ -46,7 +43,7 @@ export const projectSlicer = createSlice({
       const copyProjects = [...state.projects];
       console.log({ action });
       state.projects = copyProjects.filter(
-        (project) => project.id == action.payload.id
+        (project) => project.id !== action.payload.id
       );
     },
 
@@ -59,13 +56,28 @@ export const projectSlicer = createSlice({
       const newTaskArray = [
         ...currectProject.tasks,
         {
-          title: action.payload.taskInput,
+          id: uuidv4(),
+          startDate: new Date("2023-02-12").toDateString(),
+          ...action.payload.taskInput,
         },
       ];
       copyProjects[projecteSelectedIndex].tasks = newTaskArray;
     },
+    deleteTask: (state, action) => {
+      console.log({ action });
+      const projecteSelectedIndex = state.projects.findIndex(
+        (project) => project.id === state.selectedProjectId
+      );
+
+      const tasks = state.projects[projecteSelectedIndex].tasks;
+      state.projects[projecteSelectedIndex].tasks = tasks.filter(
+        (task) => task.id !== action.payload.id
+      );
+    },
+    editProject: (state, action) => {},
   },
 });
 
-export const { createNewProject, deleteProject } = projectSlicer.actions;
+export const { createNewProject, deleteProject, deleteTask } =
+  projectSlicer.actions;
 export default projectSlicer.reducer;
